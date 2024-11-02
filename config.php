29ed14b9c834a5a9146a3c0625fbc3fdcbd8a788
FeniksZharovik <158120171+FeniksZharovik@users.noreply.github.com>
2024-11-02 15:21:15 +0700

    ADD config

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+// config.php
+$host = 'localhost';
+$dbname = 'kabare_db';
+$username = 'root'; // sesuaikan dengan username database Anda
+$password = ''; // sesuaikan dengan password database Anda
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Koneksi gagal: " . $e->getMessage());
+}
+?>
